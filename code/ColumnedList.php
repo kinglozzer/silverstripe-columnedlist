270bcf3b7994aa6f44c3eaf97eb37f5407047e65
helpfulrobot <chris@silverstripe.com>
2015-12-18 10:53:52 +1300

    Converted to PSR-2

--- code/ColumnedList.php
+++ code/ColumnedList.php
@@ -1,54 +1,56 @@
 <?php
 
-class ColumnedList extends SS_ListDecorator {
+class ColumnedList extends SS_ListDecorator
+{
 
-	/**
-	 * @param  int $columns
-	 * @param  boolean $leftHeavy Whether the columns should be "left-heavy"
-	 * @return array
-	 */
-	public function stack($columns, $leftHeavy = true) {
-		$list = $this->list->toArray();
-		$total = count($list);
-		$partLength = floor($total / $columns);
-		$remainder = $total % $columns;
-		$result = array();
-		$offset = 0;
-		
-		// Loop over the number of columns
-		for($i = 0; $i < $columns; $i++) {
-			$handlingRemainder = ($leftHeavy) ? ($i < $remainder) : ($i >= $columns - $remainder);
+    /**
+     * @param  int $columns
+     * @param  boolean $leftHeavy Whether the columns should be "left-heavy"
+     * @return array
+     */
+    public function stack($columns, $leftHeavy = true)
+    {
+        $list = $this->list->toArray();
+        $total = count($list);
+        $partLength = floor($total / $columns);
+        $remainder = $total % $columns;
+        $result = array();
+        $offset = 0;
+        
+        // Loop over the number of columns
+        for ($i = 0; $i < $columns; $i++) {
+            $handlingRemainder = ($leftHeavy) ? ($i < $remainder) : ($i >= $columns - $remainder);
 
-			$sliceLength = ($handlingRemainder) ? $partLength + 1 : $partLength;
-			$column = array_slice($list, $offset, $sliceLength);
-			$offset += $sliceLength;
+            $sliceLength = ($handlingRemainder) ? $partLength + 1 : $partLength;
+            $column = array_slice($list, $offset, $sliceLength);
+            $offset += $sliceLength;
 
-			$result[$i] = new ArrayList($column);
-		}
+            $result[$i] = new ArrayList($column);
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
-	/**
-	 * Stacks data in columns
-	 * 
-	 * @param  int $columns The target number of columns
-	 * @param  string $children Name of the control under which children can be iterated on
-	 * @param  boolean $leftHeavy Whether the columns should be "left-heavy"
-	 * @return ArrayList
-	 */
-	public function Stacked($columns, $children = 'Children', $leftHeavy = true) {
-		$stacked = $this->stack($columns, $leftHeavy);
-		$result  = new ArrayList();
+    /**
+     * Stacks data in columns
+     * 
+     * @param  int $columns The target number of columns
+     * @param  string $children Name of the control under which children can be iterated on
+     * @param  boolean $leftHeavy Whether the columns should be "left-heavy"
+     * @return ArrayList
+     */
+    public function Stacked($columns, $children = 'Children', $leftHeavy = true)
+    {
+        $stacked = $this->stack($columns, $leftHeavy);
+        $result  = new ArrayList();
 
-		foreach($stacked as $list) {
-			$list = self::create($list);
-			$result->push(new ArrayData(array(
-				$children => $list
-			)));
-		}
+        foreach ($stacked as $list) {
+            $list = self::create($list);
+            $result->push(new ArrayData(array(
+                $children => $list
+            )));
+        }
 
-		return $result;
-	}
-
+        return $result;
+    }
 }
